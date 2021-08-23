f055552197205b6b99bf1d3301a1c7f9de083e56
121300 <121300@DESADM118>
2021-08-23 15:25:40 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Pasta lógica
+namespace HelloWorld
+{
+    class Program
+    {
+        
+        // Método Main:
+        // Ponto de entrada para a aplicação. Define o início da aplicação.
+        static void Main(string[] args)
+        {
+
+            //Imprimir na tela o "Olá  mundo!"
+            Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Meu nome é Daniel");
+
+        }
+    }
+}
